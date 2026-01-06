a8ca1cb551c3be7b48f716ccfb55444ab48ac7c9
Pavel Lopatin <pashapitch@icloud.com>
2026-01-06 21:25:16 +0500

    Initial commit

---
+++ src/main/java/com/lopatin/reminder/ReminderApplication.java
@@ -0,0 +1,13 @@
+package com.lopatin.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopatin/reminder/ReminderApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lopatin.reminder;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class ReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
