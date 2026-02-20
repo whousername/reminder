2b3858d84d491ac43331ce72ab8e70e8541e6bf7
Pavel Lopatin <pashapitch@icloud.com>
2026-02-20 20:43:56 +0500

    M2M authorization flow 200OK

--- src/main/java/com/lopatin/reminder/api/controller/ReminderController.java
+++ src/main/java/com/lopatin/reminder/api/controller/ReminderController.java
@@ -4,6 +4,7 @@ import com.lopatin.reminder.api.request.CreateReminderRequest;
 import com.lopatin.reminder.api.response.ReminderResponse;
 import com.lopatin.reminder.service.ReminderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -16,6 +17,7 @@ public class ReminderController {
 
     private final ReminderService reminderService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/reminder/create")
     public ReminderResponse createReminder(
             @RequestBody
---
+++ src/main/java/com/lopatin/reminder/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.lopatin.reminder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                List<String> roles = (List<String>) realmAccess.get("roles");
+                roles.forEach(role ->
+                        authorities.add(new SimpleGrantedAuthority("ROLE_" + role))
+                );
+            }
+            return authorities;
+        });
+        return converter;
+    }
+
+
+
+}
